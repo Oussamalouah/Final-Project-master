41ae3e0c58c88fa0c402256920f3663a25bf2231
Oussama louah <0666244774oa@gmail.com>
2022-06-06 17:00:01 +0000

    quiz

---
+++ select-rank.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" href="select-rank.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"> 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet"> 
+    <title>Document</title>
+</head>
+<body>
+ <div class="title"> 
+     <h1>Select your<br> current rank</br></h1>
+      <h3>We use this to create your plan </h3> 
+      <div class="quiz">
+        <div class="iron">
+            <img src="./img/iron.png" alt="">
+            <p>Iron</p>
+        </div>  
+        <div class="bronze">
+        <img src="./img/bronze.png" alt="">
+            <p>Bronze</p>
+        </div>    
+      </div> 
+</div>
+</body>
+</html>
