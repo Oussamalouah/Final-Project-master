9b9e4df54cc1535be87f46a48ed7b4dfab334a59
Oussamalouah <oussamalouah65@gmail.com>
2022-06-07 22:42:33 +0100

    membership

---
+++ membership.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href ="membership.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Roboto:wght@900&display=swap" rel="stylesheet">
+    <title>Pro membership</title>
+</head>
+<body>
+    <div class="container">
+      <div class="text">
+        <h1>THE ZEUSGUIDES GUARANTEE</h1>
+        <h2>Climb at least 5 divisions in 5 months, <br>or we'll give you an additional 6-month<br> membership completely free.</br></h2>
+      </div>
+       <div class ="pro">
+           <h1>PRO MEMBERSHIP</h1>
+       </div>
+    </div>
+</body>
+</html>
